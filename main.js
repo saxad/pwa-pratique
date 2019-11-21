042a02b49376c216373090250a64399fcae23cf2
saxad <saad_zizi@hotmail.com>
2019-11-21 13:29:16 +0100

    recup de cache

--- main.js
+++ main.js
@@ -17,9 +17,13 @@ function loadTechnologies(technos) {
 
 loadTechnologies(technos);
 
-// ..
+
 if(navigator.serviceWorker) {
     navigator.serviceWorker
         .register('sw.js')
         .catch(err => console.error('service worker NON enregistré', err));
 }
+
+
+
+
